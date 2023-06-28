adc0cebfb09532940d7189e539b37c81088208e2
Khang Nguyen <khang@Khangs-MacBook-Pro.local>
2023-06-28 17:19:24 +1000

    Added oil.nvim

--- dotfiles/nvim/.config/nvim/lua/core/keymaps.lua
+++ dotfiles/nvim/.config/nvim/lua/core/keymaps.lua
@@ -1,20 +1,29 @@
+-- Telescope
 local builtin = require('telescope.builtin')
-
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 
-local opts = {buffer = bufnr, remap = false}
+-- LSP
+local opts = { buffer = bufnr, remap = false }
+vim.keymap.set('n', 'gD', function() vim.lsp.buf.declaration() end, opts)
 vim.keymap.set('n', 'gd', function() vim.lsp.buf.definition() end, opts)
+vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
+vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float)
 
-vim.keymap.set('n', '<leader>e', ':NvimTreeToggle<CR>')
-vim.keymap.set('n', '<leader>ft', ':NvimTreeFocus<CR>')
+-- Harpoon
+local mark = require("harpoon.mark")
+local harpoonUi = require("harpoon.ui")
+vim.keymap.set('n', '<leader>ha', mark.add_file)
+vim.keymap.set('n', '<leader>hm', harpoonUi.toggle_quick_menu)
+vim.keymap.set('n', '<C-j>', function() harpoonUi.nav_file(1) end)
+vim.keymap.set('n', '<C-l>', function() harpoonUi.nav_file(2) end)
+vim.keymap.set('n', '<C-u>', function() harpoonUi.nav_file(3) end)
+vim.keymap.set('n', '<C-y>', function() harpoonUi.nav_file(4) end)
 
-vim.keymap.set('n', '<leader>bb', '<cmd>BufferLineCyclePrev<cr>')
-vim.keymap.set('n', '<leader>bn', '<cmd>BufferLineCycleNext<cr>')
-vim.keymap.set('n', '<leader>bj', '<cmd>BufferLinePick<cr>')
-vim.keymap.set('n', '<leader>bf', '<cmd>Telescope buffers previewer=false<cr>')
-
-vim.keymap.set('n', '<leader>bx', '<cmd>bd<cr>')
-require('leap').add_default_mappings()
+-- Misc
+vim.keymap.set("n", "-", require("oil").open, { desc = "Open parent directory" })
--- dotfiles/nvim/.config/nvim/lua/plugins/init.lua
+++ dotfiles/nvim/.config/nvim/lua/plugins/init.lua
@@ -8,18 +8,80 @@ return {
     {
         "rebelot/kanagawa.nvim",
         opts = function() return require("plugins.config.kanagawa") end
-    }, -- {
-    --     "akinsho/bufferline.nvim",
-    --     dependencies = {"nvim-tree/nvim-web-devicons"},
-    --     config = true
-    -- },
-    {"nvim-lualine/lualine.nvim", config = true},
+    }, {"nvim-lualine/lualine.nvim", config = true},
     {"nvim-treesitter/nvim-treesitter"},
-    {"nvim-tree/nvim-tree.lua", config = true},
-    {"kylechui/nvim-surround", config = true},
-    {"nvim-telescope/telescope.nvim", dependencies = {"nvim-lua/plenary.nvim"}},
+    {"kylechui/nvim-surround", config = true}, {
+        "nvim-telescope/telescope.nvim",
+        dependencies = {
+            "nvim-lua/plenary.nvim", "debugloop/telescope-undo.nvim"
+        },
+        config = function()
+            require("telescope").load_extension("undo")
+            require("telescope").setup({
+                extensions = {
+                    undo = {
+                        side_by_side = true,
+                        layout_strategy = "horizontal",
+                        layout_config = {preview_height = 0.8},
+                        mappings = {
+                            i = {
+                                ["<CR>"] = require("telescope-undo.actions").yank_additions,
+                                ["<C-d>"] = require("telescope-undo.actions").yank_deletions,
+                                ["<C-h>"] = require("telescope-undo.actions").restore
+                            }
+                        }
+                    }
+                }
+            })
+            vim.keymap.set("n", "<leader>u", "<cmd>Telescope undo<cr>")
+        end
+    }, {"ThePrimeagen/harpoon", dependencies = {"nvim-lua/plenary.nvim"}},
     {"lewis6991/gitsigns.nvim", config = true},
     {"windwp/nvim-autopairs", config = true},
-    {"numToStr/Comment.nvim", config = true},
-    {"ggandor/leap.nvim", dependencies = {"tpope/vim-repeat"}, config = true}
+    {"numToStr/Comment.nvim", config = true}, {
+        "folke/flash.nvim",
+        event = "VeryLazy",
+        ---@type Flash.Config
+        opts = {},
+        keys = {
+            {
+                "s",
+                mode = {"n", "x", "o"},
+                function()
+                    -- default options: exact mode, multi window, all directions, with a backdrop
+                    require("flash").jump()
+                end,
+                desc = "Flash"
+            }, {
+                "S",
+                mode = {"n", "o", "x"},
+                function()
+                    -- show labeled treesitter nodes around the cursor
+                    require("flash").treesitter()
+                end,
+                desc = "Flash Treesitter"
+            }, {
+                "r",
+                mode = "o",
+                function()
+                    -- jump to a remote location to execute the operator
+                    require("flash").remote()
+                end,
+                desc = "Remote Flash"
+            }, {
+                "R",
+                mode = {"n", "o", "x"},
+                function()
+                    -- show labeled treesitter nodes around the search matches
+                    require("flash").treesitter_search()
+                end,
+                desc = "Treesitter Search"
+            }
+        }
+    }, {
+        'stevearc/oil.nvim',
+        opts = {},
+        -- Optional dependencies
+        dependencies = {"nvim-tree/nvim-web-devicons"}
+    }
 }
