3c8527ddcab053312d4a282eaa631cd80ecc4287
Khang Nguyen <khang@Khangs-MacBook-Pro.local>
2023-08-25 00:55:10 +1000

    Moved jdlts to ftplugin

---
+++ dotfiles/nvim/.config/nvim/ftplugin/java.lua
@@ -0,0 +1,29 @@
+local config = {
+	cmd = {
+		os.getenv("JDTLS_JAVA_HOME"),
+		"-Declipse.application=org.eclipse.jdt.ls.core.id1",
+		"-Dosgi.bundles.defaultStartLevel=4",
+		"-Declipse.product=org.eclipse.jdt.ls.core.product",
+		"-Dlog.protocol=true",
+		"-Dlog.level=ALL",
+		"-Xmx1g",
+		"--add-modules=ALL-SYSTEM",
+		"--add-opens”, “java.base/java.util=ALL-UNNAMED",
+		"--add-opens”, “java.base/java.lang=ALL-UNNAMED",
+		"-javaagent:" .. os.getenv("JDTLS_LOMBOK_JAR"),
+		"-jar",
+		os.getenv("JDTLS_JAR"),
+		"-configuration",
+		os.getenv("JDTLS_CONFIG_PATH"),
+		"-data",
+		os.getenv("HOME") .. "/.local/share/eclipse/" .. vim.fn.fnamemodify(vim.fn.getcwd(), ":p:h:t"),
+	},
+	settings = {
+		java = {},
+	},
+	init_options = {
+		bundles = {},
+	},
+}
+
+require("jdtls").start_or_attach(config)
