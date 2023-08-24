edc469ff9ff70e308c8a061b67690e23a6392061
Khang Nguyen <khang@Khangs-MacBook-Pro.local>
2023-08-24 20:48:55 +1000

    Changed grapple keybinds

--- dotfiles/nvim/.config/nvim/lua/plugins/grapple.lua
+++ dotfiles/nvim/.config/nvim/lua/plugins/grapple.lua
@@ -1,15 +1,15 @@
-local config = function(_, opts)
+local config = function()
 	require("grapple").setup({})
 
 	vim.keymap.set("n", "<leader>ma", require("grapple").tag)
 	vim.keymap.set("n", "<leader>mm", require("grapple").popup_tags)
 
-	vim.keymap.set("n", "<C-l>", "<CMD>GrappleSelect key=1<CR>")
-	vim.keymap.set("n", "<C-u>", "<CMD>GrappleSelect key=2<CR>")
-	vim.keymap.set("n", "<C-y>", "<CMD>GrappleSelect key=3<CR>")
-	vim.keymap.set("n", "<C-p>", "<CMD>GrappleSelect key=4<CR>")
-	vim.keymap.set("n", "<C-f>", "<CMD>GrappleSelect key=5<CR>")
-	vim.keymap.set("n", "<C-w>", "<CMD>GrappleSelect key=6<CR>")
+	vim.keymap.set("n", "<leader>1", "<CMD>GrappleSelect key=1<CR>")
+	vim.keymap.set("n", "<leader>2", "<CMD>GrappleSelect key=2<CR>")
+	vim.keymap.set("n", "<leader>3", "<CMD>GrappleSelect key=3<CR>")
+	vim.keymap.set("n", "<leader>4", "<CMD>GrappleSelect key=4<CR>")
+	vim.keymap.set("n", "<leader>5", "<CMD>GrappleSelect key=5<CR>")
+	vim.keymap.set("n", "<leader>6", "<CMD>GrappleSelect key=6<CR>")
 end
 
 return {
