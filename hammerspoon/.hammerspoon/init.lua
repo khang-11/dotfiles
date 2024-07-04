44f9884a1bd199d22475a009b7e350cfe7be1d9b
Khang Nguyen <khang.nguyen@anz.com>
2024-07-05 01:32:16 +1000

    Updated hammerspoon keybindings

--- hammerspoon/.hammerspoon/init.lua
+++ hammerspoon/.hammerspoon/init.lua
@@ -1,11 +1,11 @@
 local openOrCycle = require("functions.openOrCycleApp")
 local focusWindow = require("functions.focusWindow")
 
-hs.hotkey.bind({ "control", "alt", "shift" }, "B", openOrCycle("Brave Browser"))
-hs.hotkey.bind({ "control", "alt", "shift" }, "T", openOrCycle("WezTerm"))
-hs.hotkey.bind({ "control", "alt", "shift" }, "E", openOrCycle("Microsoft Outlook"))
-hs.hotkey.bind({ "control", "alt", "shift" }, "V", openOrCycle("Microsoft Teams"))
-hs.hotkey.bind({ "control", "alt", "shift" }, "S", openOrCycle("Slack"))
+hs.hotkey.bind({ "control", "alt", "cmd", "shift" }, "B", openOrCycle("Brave Browser"))
+hs.hotkey.bind({ "control", "alt", "cmd", "shift" }, "T", openOrCycle("WezTerm"))
+hs.hotkey.bind({ "control", "alt", "cmd", "shift" }, "E", openOrCycle("Microsoft Outlook"))
+hs.hotkey.bind({ "control", "alt", "cmd", "shift" }, "V", openOrCycle("Microsoft Teams"))
+hs.hotkey.bind({ "control", "alt", "cmd", "shift" }, "S", openOrCycle("Slack"))
 
 hs.hotkey.bind({ "control", "cmd" }, "Left", focusWindow("west"))
 hs.hotkey.bind({ "control", "cmd" }, "Down", focusWindow("south"))
