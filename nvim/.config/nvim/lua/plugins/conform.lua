0f5cdeee46cf5711db91b3f86d0cc40d0bcb47a9
Khang Nguyen <khang.nguyen@anz.com>
2024-07-17 09:36:05 +1000

    Removed json formatter and added proto formatter

--- nvim/.config/nvim/lua/plugins/conform.lua
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -3,7 +3,7 @@ local config = function()
 		formatters_by_ft = {
 			go = { "gofmt", "goimports" },
 			lua = { "stylua" },
-			json = { "prettier" },
+			proto = { "buf" },
 			python = { "isort", "black" },
 			typescriptreact = { "prettierd" },
 			yaml = { "prettier" },
@@ -12,7 +12,7 @@ local config = function()
 			if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
 				return
 			end
-			return { timeout_ms = 500, lsp_fallback = true }
+			return { timeout_ms = 2000, lsp_fallback = true }
 		end,
 	})
 	vim.api.nvim_create_user_command("FormatDisable", function(args)
