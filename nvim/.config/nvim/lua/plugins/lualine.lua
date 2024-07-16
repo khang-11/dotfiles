74ba762b1ddc64c2d6377e4027a46da9a85b6daf
Khang Nguyen <khang@Khangs-MacBook-Pro.local>
2024-07-17 00:23:34 +1000

    Add inactive section lualine bar

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -8,6 +8,14 @@ local config = function()
 			lualine_y = { "branch" },
 			lualine_z = { "progress" },
 		},
+		inactive_sections = {
+			lualine_a = {},
+			lualine_b = {},
+			lualine_c = { { "filetype", icon_only = true }, { "filename", path = 1, shorting_target = 10 } },
+			lualine_x = {},
+			lualine_y = {},
+			lualine_z = {},
+		},
 	})
 end
 
