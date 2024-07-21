cfcec2930a5784a1d838bd05777a4e8866534d7d
Khang Nguyen <khang.nguyen@anz.com>
2024-07-21 16:00:50 +1000

    Update hammerspoon

--- hammerspoon/.hammerspoon/init.lua
+++ hammerspoon/.hammerspoon/init.lua
@@ -11,5 +11,3 @@ hs.hotkey.bind({ "control", "cmd" }, "Left", focusWindow("west"))
 hs.hotkey.bind({ "control", "cmd" }, "Down", focusWindow("south"))
 hs.hotkey.bind({ "control", "cmd" }, "Up", focusWindow("north"))
 hs.hotkey.bind({ "control", "cmd" }, "Right", focusWindow("east"))
-
-require("local")
