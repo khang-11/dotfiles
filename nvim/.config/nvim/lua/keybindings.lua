d11966328ab815f0e2ea47c76bffc1252c05c1a5
Khang Nguyen <khang.nguyen@anz.com>
2024-08-26 17:20:11 +1000

    Update quickfix item keybinding

--- nvim/.config/nvim/lua/keybindings.lua
+++ nvim/.config/nvim/lua/keybindings.lua
@@ -6,5 +6,5 @@ vim.keymap.set("n", "<S-Down>", "<cmd>wincmd j<cr>", { desc = "Move to below win
 vim.keymap.set("n", "<S-Up>", "<cmd>wincmd k<cr>", { desc = "Move to above window" })
 vim.keymap.set("n", "<S-Right>", "<cmd>wincmd l<cr>", { desc = "Move to right window" })
 
-vim.keymap.set("n", "<M-Down>", "<cmd>cn<cr>", { desc = "Move to next quickfix item" })
-vim.keymap.set("n", "<M-Up>", "<cmd>cp<cr>", { desc = "Move to previous quickfix item" })
+vim.keymap.set("n", "<C-Down>", "<cmd>cn<cr>", { desc = "Move to next quickfix item" })
+vim.keymap.set("n", "<C-Up>", "<cmd>cp<cr>", { desc = "Move to previous quickfix item" })
